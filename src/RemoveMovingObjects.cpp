7025e13006b55a629fe9dceca552dcb4a2917127
babahadjsaid <baba066h.s@gmail.com>
2024-05-05 23:18:52 +0200

    05/05/2024

--- src/RemoveMovingObjects.cpp
+++ src/RemoveMovingObjects.cpp
@@ -32,9 +32,10 @@ void pointCloudHandler(const lio_sam::msg::CloudInfo::SharedPtr msgIn){
 
     convertPointCloudToRangeImage(pc,currentImage_);
     cv::Mat dst;
-    cv::normalize(currentImage_, dst, 0, 2, cv::NORM_MINMAX);
+    cv::normalize(currentImage_, dst, 0, 3, cv::NORM_MINMAX);
     // cv::namedWindow("Hello world",cv::WINDOW_FULLSCREEN);
     cv::imshow("test", dst);
+    cv::imwrite("image.png",dst);
     const std::chrono::duration<double> duration = std::chrono::system_clock::now() - StartMethod;
     float durationms = 100 - 1000 * duration.count();
     std::cout <<"delay: "<< 100 - durationms <<std::endl;
@@ -49,18 +50,17 @@ void convertPointCloudToRangeImage(pcl::PointCloud<pcl::PointXYZI> &pointCloud,
 {
     if (!rangeImage_out.empty())
         rangeImage_out.release();
-    rangeImage_out.create(MAXHEIGHT, MAXWIDTH, CV_32F);
+    rangeImage_out.create(32, MAXWIDTH, CV_32F);
     // height is w 
     #pragma omp parallel for num_threads(5)
     for (auto &&point : pointCloud)
     {
-        float R, omega, alpha,x=point.y,y = -point.x;// +90 degree rotation yaw
-        R = sqrt(pow(x,2) + pow(y,2) + pow(point.z,2));
-        omega = -(asin(point.z/R) - (19.1 * TORADAIAN));
-        alpha = atan2(y,x);
+        float R, omega, alpha;
+        R = sqrt(pow(point.x,2) + pow(point.y,2) + pow(point.z,2));
+        omega = asin(point.z/R);
+        alpha = atan2(point.y,point.x);
         if (alpha < 0) alpha +=2*M_PI;
-        int i = alpha/ANGULARRESOLUTION_X , j = omega / ANGULARRESOLUTION_Y;
-        if (i<0 || j <0 || i> MAXWIDTH || j > MAXHEIGHT) std::cout<<"i: "<<i<<" j: "<<j<<" alpha: "<<asin(point.z/R)*(180.0/M_PI)<<std::endl;
+        int i = alpha/ANGULARRESOLUTION_X , j = checkRing(omega);
         #pragma omp critical
         {
             rangeImage_out.at<float>(j, i) = R;
@@ -85,6 +85,44 @@ void estimateRangeFlow(cv::Mat& rangeImage1, cv::Mat& rangeImage2, vector<cv::Ma
     cv::Sobel(rangeImage1, dy, CV_32F, 0, 1, 1);
     cv::subtract(rangeImage2, rangeImage1, dR);
     rangeFlow = {dx, dy, dR};
+}
+
+
+
+int checkRing(double angle) {
+    if (angle >= (15 * TORADAIAN))                                   return 0;
+    if (angle < (15 * TORADAIAN) && angle >= (13 * TORADAIAN))       return 1;
+    if (angle < (13 * TORADAIAN) && angle >= (11 * TORADAIAN))       return 2;
+    if (angle < (11 * TORADAIAN) && angle >= (9 * TORADAIAN))        return 3;
+    if (angle < (9 * TORADAIAN) && angle  >= (7 * TORADAIAN))        return 4;
+    if (angle < (7 * TORADAIAN) && angle  >= (5.5 * TORADAIAN))      return 5;
+    if (angle < (5.5 * TORADAIAN) && angle >= (4 * TORADAIAN))       return 6;
+    if (angle < (4 * TORADAIAN) && angle >= (2.67 * TORADAIAN))      return 7;
+    if (angle < (2.67 * TORADAIAN) && angle >= (1.33 * TORADAIAN))   return 8;
+    if (angle < (1.33 * TORADAIAN) && angle >= 0)                    return 9;
+    if (angle < 0 && angle >= (-1.33 * TORADAIAN))                   return 10;
+    if (angle < (-1.33 * TORADAIAN) && angle >= (-2.67 * TORADAIAN)) return 11;
+    if (angle < (-2.67 * TORADAIAN) && angle >= (-4 * TORADAIAN))    return 12;
+    if (angle < (-4 * TORADAIAN) && angle >= (-5.33 * TORADAIAN))    return 13;
+    if (angle < (-5.33 * TORADAIAN) && angle >= (-6.67 * TORADAIAN)) return 14;
+    if (angle < (-6.67 * TORADAIAN) && angle >= (-8 * TORADAIAN))    return 15;
+    if (angle < (-8 * TORADAIAN) && angle >= (-10 * TORADAIAN))      return 16;
+    if (angle < (-10 * TORADAIAN) && angle >= (-16 * TORADAIAN))     return 17;
+    if (angle < (-16 * TORADAIAN) && angle >= (-13 * TORADAIAN))     return 18;
+    if (angle < (-13 * TORADAIAN) && angle >= (-19 * TORADAIAN))     return 19;
+    if (angle < (-19 * TORADAIAN) && angle >= (-22 * TORADAIAN))     return 20;
+    if (angle < (-22 * TORADAIAN) && angle >= (-28 * TORADAIAN))     return 21;
+    if (angle < (-28 * TORADAIAN) && angle >= (-25 * TORADAIAN))     return 22;
+    if (angle < (-25 * TORADAIAN) && angle >= (-31 * TORADAIAN))     return 23;
+    if (angle < (-31 * TORADAIAN) && angle >= (-34 * TORADAIAN))     return 24;
+    if (angle < (-34 * TORADAIAN) && angle >= (-37 * TORADAIAN))     return 25;
+    if (angle < (-37 * TORADAIAN) && angle >= (-40 * TORADAIAN))     return 26;
+    if (angle < (-40 * TORADAIAN) && angle >= (-43 * TORADAIAN))     return 27;
+    if (angle < (-43 * TORADAIAN) && angle >= (-46 * TORADAIAN))     return 28;
+    if (angle < (-46 * TORADAIAN) && angle >= (-49 * TORADAIAN))     return 29;
+    if (angle < (-49 * TORADAIAN) && angle >= (-52 * TORADAIAN))     return 30;
+    if (angle < (-55 * TORADAIAN) )                                  return 31;
+    return 0;
 }
 
 
